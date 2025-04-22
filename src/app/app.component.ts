82760a937c2d3e019e09ebccf49a0f5fdc06c4ea
123dakalo <tshivhasedakalo7@gmail.com>
2025-04-22 10:47:43 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio-project';
+
+  isNavOpen = false;
+
+  toggleNav() {
+    this.isNavOpen = !this.isNavOpen;
+  }
 }
